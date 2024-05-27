bb13363bd4c9bb63bf56e03f77c75576d198d5ac
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-05-28 00:40:23 +0200

    chore: add initial expo files, deps & configs (#1)

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'brand-green': '#22D486',
+				'brand-black': '#323C46',
+				error: '#F40000',
+				tertiary: '#A7A7B9',
+			},
+			textColor: {
+				primary: '#000000',
+				secondary: '#72727B',
+				'primary-white': '#FFFFFF',
+			},
+			backgroundColor: {
+				primary: '#F9F9FB',
+				secondary: '#FFFFFF',
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+}
