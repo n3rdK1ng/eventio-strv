3029c9a2aa42a9e83f043afc803a3b987c6ecfd0
Marco <marco.velan10@gmail.com>
2024-05-30 16:04:13 +0200

    feat: add profile route content

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,30 +1,27 @@
-import { useRouter } from 'expo-router'
+import { ErrorBoundaryProps } from 'expo-router'
 import { View } from 'react-native'
 
-import { Button } from '#/components/button'
-import { useAuthContext } from '#/context/auth'
+import { CustomErrorBoundary } from '#/components/custom-error-boundary'
+import { EventList } from '#/components/event-list'
+import { ProfileCard } from '#/components/profile-card'
+import { SelectCardsVariant } from '#/components/select-cards-variant'
+import { Text } from '#/components/text'
+
+export function ErrorBoundary(props: ErrorBoundaryProps) {
+	return <CustomErrorBoundary {...props} />
+}
 
 export default function ProfileRoute() {
-	const { user, destroySession } = useAuthContext()
-	const router = useRouter()
-
 	return (
-		<View className="flex h-full w-full flex-col items-center justify-center gap-4 px-6">
-			<Button
-				onPress={() => {
-					console.log(user)
-				}}
-				text={'Details'}
-				className="w-full bg-brand-green"
-			/>
-			<Button
-				onPress={() => {
-					destroySession()
-					router.push('/sign-in')
-				}}
-				text={'Sign Out'}
-				className="w-full bg-brand-black"
-			/>
+		<View className="h-full w-full bg-primary">
+			<View className="px-4">
+				<ProfileCard />
+			</View>
+			<View className="flex flex-row items-center justify-between py-[10px] pl-6">
+				<Text variant="bodyLarge">My events</Text>
+				<SelectCardsVariant />
+			</View>
+			<EventList variant="profile" />
 		</View>
 	)
 }
