8ad5a124bc65cfe358f694c3033bc3889838c506
Marco <marco.velan10@gmail.com>
2024-06-02 01:08:59 +0200

    fix: improve error handling for getting events & update store

--- hooks/use-events.ts
+++ hooks/use-events.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { useAuthedFetch } from './use-authed-fetch'
 import { useEventStore } from './use-store'
@@ -6,27 +6,48 @@ import { useEventStore } from './use-store'
 export const useEvents = () => {
 	const { get } = useAuthedFetch()
 	const events = useEventStore(state => state.events)
+	const eventsFetched = useEventStore(state => state.eventsFetched)
 	const setEvents = useEventStore(state => state.setEvents)
+	const retryEventsCount = useEventStore(state => state.retryEventsCount)
+	const incrementRetryEventsCount = useEventStore(
+		state => state.incrementRetryEventsCount,
+	)
 
 	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<unknown>(null)
 
-	useEffect(() => {
-		const fetchEvents = async () => {
-			if (events.length === 0) {
-				setLoading(true)
-				try {
-					const data = await get('events')
-					setEvents(data)
-				} catch (err) {
-					throw err
-				} finally {
-					setLoading(false)
-				}
-			}
+	const fetchEvents = useCallback(async () => {
+		if (retryEventsCount > 2) {
+			return
 		}
 
-		fetchEvents()
-	}, [events.length, get, setEvents])
+		setLoading(true)
+		try {
+			const data = await get('events')
 
-	return { loading, data: events }
+			setEvents(data)
+			setLoading(false)
+		} catch (error) {
+			incrementRetryEventsCount()
+			setError(error)
+		}
+	}, [get, setEvents, incrementRetryEventsCount, retryEventsCount])
+
+	useEffect(() => {
+		if (!eventsFetched) {
+			fetchEvents()
+		}
+	}, [])
+
+	useEffect(() => {
+		if (error) {
+			// store the error in a local variable
+			// so it can be thrown after the state is cleared
+			const errorToThrow = error
+			setError(null)
+			throw errorToThrow
+		}
+	}, [error])
+
+	return { loading, data: events || [] }
 }
--- hooks/use-store.ts
+++ hooks/use-store.ts
@@ -4,15 +4,25 @@ import { TEvent } from '#/utils/api/types'
 
 type State = {
 	events: TEvent[]
-	updateEvent: (id: string, event: Partial<TEvent>) => void
+	eventsFetched: boolean
+	retryEventsCount: number
 	setEvents: (events: TEvent[]) => void
+	updateEvents: (updateFunction: (prevEvents: TEvent[]) => TEvent[]) => void
+	incrementRetryEventsCount: () => void
+	updateEvent: (id: string, event: Partial<TEvent>) => void
 }
 
 export const useEventStore = create<State>(set => ({
 	events: [],
+	eventsFetched: false,
+	retryEventsCount: 0,
+	setEvents: events => set({ events, eventsFetched: true }),
+	updateEvents: updateFunction =>
+		set(state => ({ events: updateFunction(state.events) })),
+	incrementRetryEventsCount: () =>
+		set(state => ({ retryEventsCount: state.retryEventsCount + 1 })),
 	updateEvent: (id, event) =>
 		set(state => ({
 			events: state.events.map(e => (e.id === id ? { ...e, ...event } : e)),
 		})),
-	setEvents: events => set(() => ({ events })),
 }))
