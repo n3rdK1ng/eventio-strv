16230b362bb3e04ed91a0aad6804e381b6ad8af2
Marco <marco.velan10@gmail.com>
2024-06-02 00:47:31 +0200

    fix: update icon fill color

--- components/svgs/close-icon.tsx
+++ components/svgs/close-icon.tsx
@@ -6,10 +6,8 @@ export const CloseIcon = (props: SvgProps) => {
 		<Svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
 			<Path
 				d="M6.4 19L5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6L6.4 19z"
-				fill="#72727B"
+				fill="currentColor"
 			/>
 		</Svg>
 	)
 }
-
-export default CloseIcon
