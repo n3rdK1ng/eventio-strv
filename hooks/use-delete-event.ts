6c0ac602cf5e0a9fa4d85cc6c40b973674125eda
Marco <marco.velan10@gmail.com>
2024-06-02 01:22:29 +0200

    feat: add hook for deleting events

---
+++ hooks/use-delete-event.ts
@@ -0,0 +1,39 @@
+import { useRouter } from 'expo-router'
+import { useEffect, useState } from 'react'
+
+import { useAuthedFetch } from './use-authed-fetch'
+import { useEventStore } from './use-store'
+
+export const useDeleteEvent = (eventId: string) => {
+	const router = useRouter()
+	const { del } = useAuthedFetch()
+	const { updateEvents } = useEventStore(state => state)
+
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<unknown>(null)
+
+	const fetch = async () => {
+		setLoading(true)
+		try {
+			await del(`events/${eventId}`)
+			updateEvents(events => events.filter(({ id }) => id !== eventId))
+			router.replace('/dashboard')
+		} catch (error) {
+			setError(error)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		if (error) {
+			// store the error in a local variable
+			// so it can be thrown after the state is cleared
+			const errorToThrow = error
+			setError(null)
+			throw errorToThrow
+		}
+	}, [error])
+
+	return { loading, deleteEvent: fetch }
+}
