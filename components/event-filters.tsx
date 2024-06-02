567dd47a692065a0d22aea33e7b09b2dfcccbaf8
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-06-02 02:31:28 +0200

    refactor: use global params in event filters (#36)

--- components/event-filters.tsx
+++ components/event-filters.tsx
@@ -1,50 +1,63 @@
-import { useState } from 'react'
+import { useGlobalSearchParams, useRouter } from 'expo-router'
+import { useEffect } from 'react'
 import { View } from 'react-native'
 
 import { cn } from '#/utils/misc'
 
 import { Button } from './button'
 
+type TFilterButton = {
+	filter: 'all' | 'future' | 'past'
+	currentFilter: 'all' | 'future' | 'past'
+	onPress: () => void
+}
+
+export type TFilter = 'all' | 'future' | 'past'
+
+const FilterButton = ({ filter, currentFilter, onPress }: TFilterButton) => (
+	<Button
+		text={filter}
+		variant="large"
+		textVariant="overlineSmall"
+		className={cn(
+			filter === 'future' ? 'mx-2 h-8 w-1/3' : 'h-8 w-1/3',
+			currentFilter === filter ? 'bg-brand-black' : 'bg-secondary',
+		)}
+		textColor={currentFilter === filter ? 'primary-white' : 'tertiary'}
+		onPress={onPress}
+	/>
+)
+
 export const EventFilters = () => {
-	const [selectedFilter, setSelectedFilter] = useState<
-		'all' | 'future' | 'past'
-	>('all')
+	const router = useRouter()
+	const { eventFilter } = useGlobalSearchParams() as {
+		eventFilter: TFilter
+	}
+
+	useEffect(() => {
+		if (!eventFilter) {
+			router.setParams({
+				eventFilter: 'all',
+			})
+		}
+	}, [])
+
+	const handlePress = (filter: TFilter) => () => {
+		router.setParams({
+			eventFilter: filter,
+		})
+	}
 
 	return (
-		<View className="flex w-full flex-row justify-center bg-primary pt-6 px-10">
-			<Button
-				text="all"
-				variant="large"
-				textVariant="overlineSmall"
-				className={cn(
-					'h-8 w-1/3',
-					selectedFilter === 'all' ? 'bg-brand-black' : 'bg-secondary',
-				)}
-				textColor={selectedFilter === 'all' ? 'primary-white' : 'tertiary'}
-				onPress={() => setSelectedFilter('all')}
-			/>
-			<Button
-				text="future"
-				variant="large"
-				textVariant="overlineSmall"
-				className={cn(
-					'h-8 w-1/3 mx-2',
-					selectedFilter === 'future' ? 'bg-brand-black' : 'bg-secondary',
-				)}
-				textColor={selectedFilter === 'future' ? 'primary-white' : 'tertiary'}
-				onPress={() => setSelectedFilter('future')}
-			/>
-			<Button
-				text="past"
-				variant="large"
-				textVariant="overlineSmall"
-				className={cn(
-					'h-8 w-1/3',
-					selectedFilter === 'past' ? 'bg-brand-black' : 'bg-secondary',
-				)}
-				textColor={selectedFilter === 'past' ? 'primary-white' : 'tertiary'}
-				onPress={() => setSelectedFilter('past')}
-			/>
+		<View className="flex w-full flex-row justify-center bg-primary px-10 pt-6">
+			{(['all', 'future', 'past'] as const).map(filter => (
+				<FilterButton
+					key={filter}
+					filter={filter}
+					currentFilter={eventFilter}
+					onPress={handlePress(filter)}
+				/>
+			))}
 		</View>
 	)
 }
